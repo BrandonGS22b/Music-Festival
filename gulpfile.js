d80e4a497b2c631c8b399521d8245f162d039487
BRANDON_GARCIA <159663607+BrandonGS22b@users.noreply.github.com>
2024-06-21 11:33:06 -0500

    scss
    estaremos trabajando con este de globales este archivo.
    
    Voy a hacer esto más pequeño.
    
    Y de momento este ya lo voy a cerrar.
    
    Este archivo nos va a permitir definir algunos estilos generales que se van a aplicar en todo el proyecto.

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ const sass = gulpSass(dartSass);
 
 export function css(done){
     src('src/scss/app.scss') //esto ubica al archivo donde hacamos nuestras modificacion del css
-        .pipe(sass()) //aplicamos sass
+        .pipe(sass().on('error', sass.logError)) //aplicamos sass y le agregamos  un .logerror ya que la consola no nos detecta todos los errores
         .pipe(dest('build/css'))//dest donde queremos almacenar ese archivo osea la modificacion del app.scss
 
     done();
@@ -18,7 +18,10 @@ export function css(done){
 
 //implementamos el whatch para no estar levantando a cada rato el servicio
 export function dev(){
-    watch('src/scss/app.scss', css); //bucamos el archivo y llamamos la function css que esta arriba
+    console.log('Iniciando watch para cambios en SCSS...');
+    watch('src/scss/**/*.scss', css); //bucamos el archivo y llamamos la function css que esta arriba
+
+    //ese patron me permite bucar todo los archivos con ** y despues del /* me busca todos los archivos con .css
 }
 
 
