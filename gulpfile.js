8e932cbf8662f80abdbf568d0b1d66e696dca0d3
BRANDON_GARCIA <159663607+BrandonGS22b@users.noreply.github.com>
2024-06-21 02:51:59 -0500

    compilar sass con gulp
    6.compilar sass con gulp
    1.instalamos dependencia
    -npm i --save-dev gulp-sass
    
    6.1 vamos ya no vamos a levantar el servicio desde package.json vamos agregar dos function css y wacht para levantarlo desde gulpfile.js=
    
    import {src,dest, watch} from 'gulp';
    //este es el src:sass --watch src
    //y este el dest :scss:build/css
    import gulpSass  from 'gulp-sass';
    import * as dartSass from 'sass';
    
    //unimos los dos imports gulpSass y * as dartSass
    const sass = gulpSass(dartSass);
    
    export function css(done){
        src('src/scss/app.scss') //esto ubica al archivo donde hacamos nuestras modificacion del css
            .pipe(sass()) //aplicamos sass
            .pipe(dest('build/css'))//dest donde queremos almacenar ese archivo osea la modificacion del app.scss
    
        done();
    }
    
    //implementamos el whatch para no estar levantando a cada rato el servicio
    export function dev(){
        watch('src/scss/app.scss', css); //bucamos el archivo y llamamos la function css que esta arriba
    }
    
    //todo esto es lo misto que esto que esta en el package.json "sass": "sass --watch src/scss:build/css",

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,25 @@
-function hola(done){
-    console.log("hola desde gulpfile.js");
-    done();//esto nos dice que  ya finalizo la tarea
+import {src,dest, watch} from 'gulp';
+//este es el src:sass --watch src
+//y este el dest :scss:build/css
+import gulpSass  from 'gulp-sass';
+import * as dartSass from 'sass';
+
+
+//unimos los dos imports gulpSass y * as dartSass
+const sass = gulpSass(dartSass);
+
+export function css(done){
+    src('src/scss/app.scss') //esto ubica al archivo donde hacamos nuestras modificacion del css
+        .pipe(sass()) //aplicamos sass
+        .pipe(dest('build/css'))//dest donde queremos almacenar ese archivo osea la modificacion del app.scss
+
+    done();
 }
+
+//implementamos el whatch para no estar levantando a cada rato el servicio
+export function dev(){
+    watch('src/scss/app.scss', css); //bucamos el archivo y llamamos la function css que esta arriba
+}
+
+
+//todo esto es lo misto que esto que esta en el package.json "sass": "sass --watch src/scss:build/css",
